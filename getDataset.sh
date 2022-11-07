43ee409d110c76b346388923bb3dbc78a02b2b73
Darkcustard <finn.brouwer@gmail.com>
2022-11-07 21:43:21 +0200

    bugfix

--- getDataset.sh
+++ getDataset.sh
@@ -1,7 +1,7 @@
 curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/The%20Eiffel%20Tower.npy" --output 'src/train/eiffel.npy'
 curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/airplane.npy" --output 'src/train/airplane.npy'
-curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/banana.npy" --ouput 'src/train/banana.npy'
-curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/bee.npy" --ouput 'src/train/bee.npy'
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/banana.npy" --output 'src/train/banana.npy'
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/bee.npy" --output 'src/train/bee.npy'
 curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/bicycle.npy" --output 'src/train/bicycle.npy'
 curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/bulldozer.npy" --output 'src/train/bulldozer.npy'
 
