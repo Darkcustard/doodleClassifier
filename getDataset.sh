df712c7d3871ad0b3224711fe95f1e6cb29567fd
Darkcustard <finn.brouwer@gmail.com>
2022-11-07 21:42:17 +0200

    Dataset downloader complete, gitignore in place.

---
+++ getDataset.sh
@@ -0,0 +1,8 @@
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/The%20Eiffel%20Tower.npy" --output 'src/train/eiffel.npy'
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/airplane.npy" --output 'src/train/airplane.npy'
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/banana.npy" --ouput 'src/train/banana.npy'
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/bee.npy" --ouput 'src/train/bee.npy'
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/bicycle.npy" --output 'src/train/bicycle.npy'
+curl "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/bulldozer.npy" --output 'src/train/bulldozer.npy'
+
+echo 'Dataset Download Complete.'
