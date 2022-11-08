ed1110866e801f1da15609e00b4bee016083dbfc
Darkcustard <finn.brouwer@gmail.com>
2022-11-08 13:37:08 +0200

    training script up and running

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Input
+from tensorflow.keras import metrics
 
 dataDictionary = {
 
@@ -18,6 +19,9 @@ def loadDataset(path:str, labelName:str):
 
     x = np.load(path)
     length = x.shape[0]
+    x = np.reshape(x,(length,28,28))
+    print(x.shape)
+
     label = dataDictionary[labelName]    
     y = np.tile(label,(length,1))
 
@@ -34,6 +38,52 @@ bulldozerX, bulldozerY = loadDataset('src/train/bulldozer.npy','bulldozer')
 
 def buildClassifier():
 
-    model = Sequential()
+    #define model
+    print('building model...')
+    
+    classifier = Sequential()
 
-    return model
+    #input
+    classifier.add(Input(shape=(28,28,1)))
+
+    #hidden
+    classifier.add(Conv2D(32,(3,3),padding="same"))
+    classifier.add(MaxPooling2D(pool_size=(2,2),padding="same"))
+    classifier.add(Dropout(0.1))
+    
+    classifier.add(Conv2D(32,(3,3),padding="same"))
+    classifier.add(MaxPooling2D(pool_size=(2,2),padding="same"))
+    
+    classifier.add(Flatten())
+    
+    classifier.add(Dense(72,activation="relu"))
+    classifier.add(Dense(36,activation="relu"))
+
+    #output
+    classifier.add(Dense(6, activation="softmax"))
+    
+    
+    #compile
+    classifier.compile(loss = 'binary_crossentropy', run_eagerly=True ,optimizer = 'adam', metrics=[metrics.CategoricalAccuracy()])
+    
+    return classifier
+
+
+
+def trainClassifier(classifier, epochs:int, batch_size:int):
+
+    for x in range(epochs):
+
+        print(f'Starting Epoch: {x+1}')
+        classifier.fit(airplaneX,airplaneY,epochs=1,batch_size=batch_size)
+        classifier.fit(beeX,beeY,epochs=1,batch_size=batch_size)
+        classifier.fit(eiffelX,eiffelY,epochs=1,batch_size=batch_size)
+        classifier.fit(bulldozerX,bulldozerY,epochs=1,batch_size=batch_size)
+        classifier.fit(bananaX,bananaY,epochs=1,batch_size=batch_size)
+        classifier.fit(bicycleX,bicycleY,epochs=1,batch_size=batch_size)
+
+    print("Saving model.")
+    classifier.save('classifier.ai')
+
+classifier = buildClassifier()
+trainClassifier(classifier,1,256)
