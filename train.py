51f597f40cfd71ad3b4d7a5af0dfef1c3b6b48b5
Darkcustard <finn.brouwer@gmail.com>
2022-11-09 21:29:39 +0200

    added Vram limitation functionality

--- train.py
+++ train.py
@@ -1,8 +1,27 @@
 import numpy as np
 
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Input
 from tensorflow.keras import metrics
+
+
+limitVram = True
+
+if limitVram:
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
 
 dataDictionary = {
 
@@ -142,4 +161,4 @@ def spliceData():
 
 x,y = spliceData()
 classifier = buildClassifier()
-trainClassifier(classifier,5,5,x,y)
+trainClassifier(classifier,5,50,x,y)
